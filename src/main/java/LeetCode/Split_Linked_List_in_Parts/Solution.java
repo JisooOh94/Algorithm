c23e5b619357a57207da2b8b955a1d41aba6d41d
jisoooh <jisoooh94@worksmobile.com>
2020-01-19 20:24:45 +0900

    [LeetCode] 725. Split Linked List in Parts * Time Complexity : O(n) * Trial Count : 1 * Result : Success * Performace    * Time : 100.00%    * Memory : 7.69%

--- src/main/java/LeetCode/Split_Linked_List_in_Parts/Solution.java
+++ src/main/java/LeetCode/Split_Linked_List_in_Parts/Solution.java
@@ -1,58 +1,13 @@
 package LeetCode.Split_Linked_List_in_Parts;
 
+/**
+ * Time Complexity : O(n)
+ */
 public class Solution {
 	public static class ListNode {
 	    int val;
 	    ListNode next;
 	    ListNode(int x) { val = x; }
-	}
-
-
-	public static ListNode[] splitListToParts(ListNode root, int partCnt) {
-		ListNode[] resultList = new ListNode[partCnt];
-		ListNode cur;
-		int size = 0;
-
-		cur = root;
-		while(cur != null) {
-			size++;
-			cur = cur.next;
-		}
-
-		int baseSize = size / partCnt;
-
-		if(baseSize == 0) {
-			cur = root;
-			for(int i = 0; i < partCnt; i++) {
-				if(i >= size) {
-					resultList[i] = null;
-				} else {
-					resultList[i] = new ListNode(cur.val);
-					cur = cur.next;
-				}
-			}
-			return resultList;
-		}
-
-		int leftSize = size % partCnt;
-
-		cur = root;
-		for(int outerIdx = 0; outerIdx < partCnt; outerIdx++) {
-			int partSize = outerIdx < leftSize ? baseSize + 1 : baseSize;
-
-			ListNode partCur = new ListNode(cur.val);
-			resultList[outerIdx] = partCur;
-			cur = cur.next;
-
-			for(int innerIdx = 0; innerIdx < partSize - 1; innerIdx++) {
-				ListNode newNode = new ListNode(cur.val);
-				partCur.next = newNode;
-				partCur = partCur.next;
-				cur = cur.next;
-			}
-		}
-
-		return resultList;
 	}
 
 	public static ListNode[] splitListToParts_2(ListNode root, int partCnt) {
