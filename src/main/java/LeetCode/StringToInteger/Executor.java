c7499d270f214b6280723482613ec2d89f400fae
jisoooh <jisoooh94@worksmobile.com>
2020-01-27 17:40:45 +0900

    [LeetCode] 8. String to Integer (atoi) * Time Complexity : O(n) * Trial Count : 1 * Result : Success * Performace    * Time : 3ms, 30.84%    * Memory : 40mb, 5.59%

---
+++ src/main/java/LeetCode/StringToInteger/Executor.java
@@ -0,0 +1,14 @@
+package LeetCode.StringToInteger;
+
+import org.junit.Test;
+import LeetCode.StringToInteger.Solution;
+import util.PerformanceUtil;
+
+public class Executor {
+	@Test
+	public void test() {
+		String input = "";
+
+		PerformanceUtil.calcPerformance(new Solution(), (Object)input);
+	}
+}
