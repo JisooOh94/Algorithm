439210edaec44d2fc091d809129d3c073852a62d
jisoooh <jisoooh94@worksmobile.com>
2020-03-08 00:16:06 +0900

    [LeetCode] 19. Remove Nth Node From End of List

---
+++ src/main/java/LeetCode/Remove_Nth_Node/Solution_2.java
@@ -0,0 +1,71 @@
+package LeetCode.Remove_Nth_Node;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import util.ListNode;
+import util.Predicate;
+
+/**
+ * Runtime : 16ms (14.24%)
+ * Memory : 40.7 mb (6.37%)
+ */
+public class Solution_2 implements Predicate<List, Object> {
+	public List test(Object[] params ) {
+		List<Integer> resultList = new LinkedList();
+		ListNode head = removeNthFromEnd((ListNode)params[0], (int)params[1]);
+		ListNode cur = head;
+		while(cur != null) {
+			resultList.add(cur.val);
+			cur = cur.next;
+		}
+
+		return resultList;
+	}
+
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		if(head == null || head.next == null) {
+			return null;
+		} else if(n == 0) {
+			return head;
+		}
+
+		ListNode cur = head;
+		if(n == 1) {
+			ListNode prev = cur;
+			while(cur.next != null) {
+				prev = cur;
+				cur = cur.next;
+			}
+			prev.next = null;
+			return head;
+		}
+
+		int initialSize = 1000000;
+		ListNode[] nodeArr = new ListNode[initialSize];
+		int nodeCnt = 0;
+
+		while(cur != null) {
+			nodeArr[nodeCnt++] = cur;
+			if(nodeCnt == nodeArr.length) {
+				ListNode[] newArr = new ListNode[nodeCnt + initialSize];
+				System.arraycopy(nodeArr, 0, newArr, 0, nodeCnt);
+				nodeArr = newArr;
+			}
+
+			cur = cur.next;
+		}
+
+		int targetIdx = nodeCnt - n;
+
+		if(targetIdx == 0) {
+			head = head.next;
+		} else if(targetIdx == nodeCnt - 1) {
+			nodeArr[targetIdx - 1].next = null;
+		} else {
+			nodeArr[targetIdx - 1].next = nodeArr[targetIdx + 1];
+		}
+
+		return head;
+	}
+}
---
+++ src/main/java/LeetCode/Remove_Nth_Node/Solution_3.java
@@ -0,0 +1,75 @@
+package LeetCode.Remove_Nth_Node;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import util.ListNode;
+import util.Predicate;
+
+/**
+ * Runtime : 0ms (100%)
+ * Memory : 38.1 mb (6.37%)
+ */
+public class Solution_3 implements Predicate<List, Object> {
+	public class ReverseNode {
+		public ListNode node;
+		public ReverseNode prev;
+
+		public ReverseNode(ListNode node, ReverseNode prev) {
+			this.node = node;
+			this.prev = prev;
+		}
+	}
+	public List test(Object[] params ) {
+		List<Integer> resultList = new LinkedList();
+		ListNode head = removeNthFromEnd((ListNode)params[0], (int)params[1]);
+		ListNode cur = head;
+		while(cur != null) {
+			resultList.add(cur.val);
+			cur = cur.next;
+		}
+
+		return resultList;
+	}
+
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		if(head == null || head.next == null) {
+			return null;
+		} else if(n == 0) {
+			return head;
+		}
+
+		ListNode cur = head;
+		if(n == 1) {
+			ListNode prev = cur;
+			while(cur.next != null) {
+				prev = cur;
+				cur = cur.next;
+			}
+			prev.next = null;
+			return head;
+		}
+
+		int nodeCnt = 0;
+		ReverseNode rNode = new ReverseNode(cur, null);
+		while(cur != null) {
+			cur = cur.next;
+			ReverseNode temp = new ReverseNode(cur, rNode);
+			rNode = temp;
+			nodeCnt++;
+		}
+
+		if(nodeCnt == n) {
+			head = head.next;
+			return head;
+		}
+
+		for(int i = 0; i < n - 1; i++) {
+			rNode = rNode.prev;
+		}
+
+		rNode.prev.prev.node.next = rNode.node;
+
+		return head;
+	}
+}
