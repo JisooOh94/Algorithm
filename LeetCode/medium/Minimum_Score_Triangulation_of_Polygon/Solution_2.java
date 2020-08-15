35f3a0d40b9033c14e9d1b1e9acc89e001b2c87f
Oh Ji Soo <48702893+JisooOh94@users.noreply.github.com>
2020-08-16 00:12:57 +0900

    [LeetCode] 1039. Minimum Score Triangulation of Polygon

--- LeetCode/medium/Minimum_Score_Triangulation_of_Polygon/Solution_2.java
+++ LeetCode/medium/Minimum_Score_Triangulation_of_Polygon/Solution_2.java
@@ -2,6 +2,10 @@ package medium.Minimum_Score_Triangulation_of_Polygon;
 
 import org.junit.Test;
 
+/**
+ * Runtime : 4ms(39.52%)
+ * Memory : 39.5mb(5.47%)
+ */
 public class Solution_2 {
 	@Test
 	public void execute() {
@@ -11,29 +15,30 @@ public class Solution_2 {
 //		int[] arr = new int[]{4,3,4,3,5};
 //		int[] arr= new int[]{2,2,2,2,1};
 //		int[] arr = new int[]{5,5,5,4,2,5};
-		int[] arr = new int[]{1,2,3,4,5};
+//		int[] arr = new int[]{1,2,3,4,5};
+		int[] arr = new int[]{38,76,69,32,24,35,82,30,86,77,92,3,35,20,84,67,23,58,94,10};
 		System.out.println(minScoreTriangulation(arr));
 	}
 
-	private int recursion(int start, int end, int[] arr) {
+	private int recursion(int start, int end, int[] arr, int[][] record) {
 		if(Math.abs(start - end) == 1) return 0;
+		if(record[start][end] != 0) return record[start][end];
+		int min = 999999999;
 
-		int range = start < end ? end - start : end + arr.length - start;
-		int min = 99999;
+		for(int i = start + 1; i < end; i++) {
+			int cur = arr[start] * arr[i] * arr[end];
+			cur += recursion(start, i, arr,record) + recursion(i, end, arr, record);
 
-		for(int p1 = 0; p1 < range; p1++) {
-			int p2 = p1 + 1;
-			for(int p3 = p2 + 1; p3 < p1 + arr.length; p3++) {
-				int cur = arr[start + p1] * arr[(start + p2) % arr.length] * arr[(start + p3) % arr.length];
-				cur += recursion(start + p2, start + p3, arr) + recursion(start + p3, start + p1, arr);
-				min = Math.min(min, cur);
-			}
+			min = Math.min(min, cur);
 		}
+
+		record[start][end] = min;
 
 		return min;
 	}
 
 	public int minScoreTriangulation(int[] arr) {
-		return recursion(0, arr.length, arr);
+		int[][] record = new int[arr.length][arr.length];
+		return recursion(0, arr.length - 1, arr, record);
 	}
 }
