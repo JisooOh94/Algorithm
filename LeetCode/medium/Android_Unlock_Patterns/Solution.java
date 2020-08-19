b4f7dad280934d7e898c99779cc3b1a4c35ab55d
JisooOh94 <chocokon@gmail.com>
2020-08-19 12:16:26 +0900

    [LeetCode] 351. Android Unlock Patterns

---
+++ LeetCode/medium/Android_Unlock_Patterns/Solution.java
@@ -0,0 +1,52 @@
+package medium.Android_Unlock_Patterns;
+
+import org.junit.Test;
+
+public class Solution {
+	private static final int[][] pos = new int[][]{
+			{0, 0},
+			{0, 1},
+			{0, 2},
+			{1, 0},
+			{1, 1},
+			{1, 2},
+			{2, 0},
+			{2, 1},
+			{2, 2}
+	};
+
+	@Test
+	public void execute() {
+		int m = 1; int n = 1;
+		System.out.println(numberOfPatterns(m, n));
+	}
+
+	private boolean isNotOverlapped(int idx_1, int idx_2) {
+		return Math.abs(pos[idx_1][0] - pos[idx_2][0]) < 2 && Math.abs(pos[idx_1][1] - pos[idx_2][1]) < 2;
+	}
+
+	private int recursion(int curIdx, int curCnt, boolean[] visited, int min, int max) {
+		if(curCnt >= max) return 1;
+		int patCnt = 0;
+
+		for(int i = 0; i < 9; i++) {
+			if(!visited[i] && isNotOverlapped(curIdx, i)) {
+				visited[i] = true;
+				patCnt += recursion(i, curCnt + 1, visited, min, max);
+				visited[i] = false;
+			}
+		}
+
+		return min <= curCnt ? patCnt + 1 : patCnt;
+	}
+	public int numberOfPatterns(int m, int n) {
+		boolean[] visited = new boolean[9];
+		int patCnt = 0;
+		for(int i = 0; i < 9; i++) {
+			visited[i] = true;
+			patCnt += recursion(i, 1, visited, m, n);
+			visited[i] = false;
+		}
+		return patCnt;
+	}
+}
