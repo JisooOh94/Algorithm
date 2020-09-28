dcb512ac50c8c8e0545570f297525b861e9ef5a5
JisooOh94 <chocokon@gmail.com>
2020-09-29 01:00:04 +0900

    [LeetCode] 1490. Clone N-ary Tree

---
+++ LeetCode/medium/Clone_N_ary_Tree/Solution.java
@@ -0,0 +1,26 @@
+package medium.Clone_N_ary_Tree;
+
+import java.util.List;
+
+import util.ListNode;
+
+/**
+ * Runtime : 1ms(93.04%)
+ * Memory : 40.7mb(97.76%)
+ */
+public class Solution {
+	private ListNode recurion(ListNode cur) {
+		ListNode copy = new ListNode(cur.val);
+		List<ListNode> childList = copy.children;
+
+		for(ListNode child : cur.children) {
+			childList.add(recurion(child));
+		}
+
+		return copy;
+	}
+	public ListNode cloneTree(ListNode root) {
+		if(root == null) return null;
+		return recurion(root);
+	}
+}
